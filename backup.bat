dfe0d02daa2ed17db6eb988b4298125ae56ac99f
Ikuya Tanaka <pttp730@gmail.com>
2018-10-12 22:18:28 +0900

    html出力に対応。

--- backup.bat
+++ backup.bat
@@ -1,4 +1,10 @@
 @echo off
-set /P ACCOUNT="Please enter the Qiita account to be back up: "
-python qiita_backupper.py %ACCOUNT%
+
+rem Qiita account name
+set ACCOUNT=i-tanaka730
+
+rem Output format (all / md / html)
+set FORMAT=all
+
+python qiita_backupper.py %ACCOUNT% %FORMAT%
 pause
--- qiita_backupper.py
+++ qiita_backupper.py
@@ -5,6 +5,14 @@ import os
 import sys
 import requests
 import re
+import urllib.request
+import urllib.parse
+
+# フォーマットチェック
+def check_format():
+    if FORMAT not in ['all', 'md', 'html']:
+        print('Error : Please check the output format.')
+        sys.exit(1)
 
 # レスポンスチェック
 def check_response(response):
@@ -23,24 +31,51 @@ def make_backup_dir():
 def save(item):
     title = exclude_unavailable_filename(item["title"])
     body  = item['body'].encode('utf-8')
-    filename = '{0}.md'.format(title)
-    fullpath = os.path.join(BUCKUP_DIR, filename)
 
-    with open(fullpath, 'wb') as f:
-        f.write(body)
+    if FORMAT in ['all', 'md']:
+        save_markdown(title, body)
 
-    print ('[OK!] ' + filename)
+    if FORMAT in ['all', 'html']:
+        save_html(title, body)
+
+    print ('[OK!] ' + title)
 
 # ファイル名に使用できない文字を除外
 def exclude_unavailable_filename(name):
     return re.sub(r'[\\|/|:|?|.|"|<|>|\|]', '', name)
 
+# markdown形式で保存
+def save_markdown(title, body):
+    filename = '{0}.md'.format(title)
+    filepath = os.path.join(BUCKUP_DIR, filename)
+
+    with open(filepath, 'wb') as file:
+        file.write(body)
+
+# html形式で保存
+def save_html(title, body):
+    filename = '{0}.html'.format(title)
+    filepath = os.path.join(BUCKUP_DIR, filename)
+
+    request = urllib.request.Request(HTML_CONVERT_URL)
+    request.add_header('Content-Type', 'text/plain')
+    f = urllib.request.urlopen(request, body)
+
+    with open(filepath,'bw') as file:
+        file.write(f.read())
+
+
+ACCOUNT = sys.argv[1]
+FORMAT = sys.argv[2]
 # バックアップするQiitaアカウントのURL
-URL = 'https://qiita.com/api/v2/users/' + sys.argv[1] + '/items'
+QIITA_ACCOUNT_URL = 'https://qiita.com/api/v2/users/' + ACCOUNT + '/items'
 # バックアップ先のフォルダのパス
 BUCKUP_DIR = os.path.abspath(os.path.dirname(__file__)) + '/backup'
+# markdownをhtmlに変換するGitHubApiのURL
+HTML_CONVERT_URL = 'https://api.github.com/markdown/raw'
 
-response = requests.get(URL)
+check_format()
+response = requests.get(QIITA_ACCOUNT_URL)
 check_response(response)
 make_backup_dir()
 items = response.json()
